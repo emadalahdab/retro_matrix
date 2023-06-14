65ce4ba3003ffe594e9b8a775a13a10bba187ef3
emadalahdab <43016140+emadalahdab@users.noreply.github.com>
2023-06-14 16:41:41 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python /home/pi/Desktop/test.py
 python /home/pi/Desktop/retro_matrix.resetmatrix.py
-/home/pi/Desktop/retro_matrix/run_retro_matrix --led-slowdown-gpio=4
+/home/pi/Desktop/retro_matrix/run_retro_matrix --led-slowdown-gpio=4 --led-gpio-mapping=regular --led-no-hardware-pulse --led-parallel=2 --led-cols=64 --led-rows=32
 
